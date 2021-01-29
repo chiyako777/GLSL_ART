1cca01b16042dba007ad0080c7ab9e4ee025eac9
chiyako777 <chi2324ya8388ko108500@gmail.com>
2021-01-29 15:17:18 +0900

    20210129_1517

--- draw.cpp
+++ draw.cpp
@@ -364,8 +364,8 @@ void draw20210126(int initFlg) {
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 		//** シェーダー読み込み
-		std::string vShaderFileName = R"#(GLSL/202101/20210129_6.vert)#";
-		std::string fShaderFileName = R"#(GLSL/202101/20210129_6.frag)#";
+		std::string vShaderFileName = R"#(GLSL/202101/20210129_7.vert)#";
+		std::string fShaderFileName = R"#(GLSL/202101/20210129_7.frag)#";
 
 		programId = createShader(vShaderFileName, fShaderFileName);
 
